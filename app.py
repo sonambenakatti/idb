f3fc631eeb25a9c2ce785426dfda9571a3766d3f
Amrutha Sreedharane <amrutha.sreedharane@utexas.edu>
2018-02-09 14:07:46 -0600

    Scrapped scenic locations from google api and displayed the instances on the locations page

--- app.py
+++ app.py
@@ -42,11 +42,30 @@ APP = flask.Flask(__name__)
 
 @APP.route('/')
 def index():
-    """ Displays the index page accessible at '/'
+    """ Displays the index page accessible at
     """
     return flask.render_template('home.html')
 
+@APP.route('/scenic')
+def sceniclocations():
+	r1 = requests.get('https://maps.googleapis.com/maps/api/place/textsearch/json?type=park&location=30.267153,-97.7430608&radius=10000&key=AIzaSyD_XJoBX5jhOnTthWnFc1gzJYQ3sumP-pk')
+	json1 = r1.json()
+	print(json1)
+	name1 = json1["results"][0]["name"]
+	location1 = json1["results"][0]["formatted_address"]
+	photor1 = 'https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=' + json1['results'][0]['photos'][0]['photo_reference']+ '&key=AIzaSyD_XJoBX5jhOnTthWnFc1gzJYQ3sumP-pk'
+
+	name2 = json1["results"][1]["name"]
+	location2 = json1["results"][1]["formatted_address"]
+	photor2 = 'https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=' + json1['results'][1]['photos'][0]['photo_reference']+ '&key=AIzaSyD_XJoBX5jhOnTthWnFc1gzJYQ3sumP-pk'
+
+	
+	name3 = json1["results"][2]["name"]
+	location3 = json1["results"][2]["formatted_address"]
+	photor3 = 'https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=' + json1['results'][2]['photos'][0]['photo_reference']+ '&key=AIzaSyD_XJoBX5jhOnTthWnFc1gzJYQ3sumP-pk'
+	return flask.render_template('products.html', name1=name1, location1=location1, photo1=photor1, name2=name2, location2=location2, photo2=photor2, name3=name3, location3=location3, photo3=photor3)
+
 
 if __name__ == '__main__':
-    APP.debug=True
+    #APP.debug=True
     APP.run()
