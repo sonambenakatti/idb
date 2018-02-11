c39dc702882b77745663a694576c4ee11120f27b
Amrutha Sreedharane <amrutha.sreedharane@utexas.edu>
2018-02-11 15:37:42 -0600

    Fixed CSS and attempted embedding maps

--- app.py
+++ app.py
@@ -26,32 +26,37 @@ def index():
 
 @APP.route('/scenic')
 def sceniclocations():
-	r1 = requests.get('https://maps.googleapis.com/maps/api/place/textsearch/json?type=park&location=30.267153,-97.7430608&radius=10000&key=AIzaSyD_XJoBX5jhOnTthWnFc1gzJYQ3sumP-pk')
+	r1 = requests.get('https://maps.googleapis.com/maps/api/place/textsearch/json?type=park&location=30.267153,-97.7430608&radius=10000&key=AIzaSyBlOaCDL8ePD3nignTrJN1oViXj_rDx_1U')
 	json1 = r1.json()
 	
 	print(json1)
 	try:
 		rating1 =  json1["results"][0]["rating"]
-		rating2 =  json1["results"][1]["rating"]
-		rating3 =  json1["results"][2]["rating"]
-
-
 
 	except:
 		rating1 = 0
+
+	try:
+		rating2 =  json1["results"][1]["rating"]
+	except:
 		rating2 = 0
+
+	try:
+		rating3 =  json1["results"][2]["rating"]
+	except:
+
 		rating3 = 0
 
 	name1 = json1["results"][0]["name"]
 		#location1 = json1["results"][0]["formatted_address"]
 		#location1 = json1["results"][0]["formatted_address"]
 
-	photor1 = 'https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=' + json1['results'][0]['photos'][0]['photo_reference']+ '&key=AIzaSyD_XJoBX5jhOnTthWnFc1gzJYQ3sumP-pk'
+	photor1 = 'https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=' + json1['results'][0]['photos'][0]['photo_reference']+ '&key=AIzaSyBlOaCDL8ePD3nignTrJN1oViXj_rDx_1U'
 
 	name2 = json1["results"][1]["name"]
 		#location2 = json1["results"][1]["formatted_address"]
 
-	photor2 = 'https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=' + json1['results'][1]['photos'][0]['photo_reference']+ '&key=AIzaSyD_XJoBX5jhOnTthWnFc1gzJYQ3sumP-pk'
+	photor2 = 'https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=' + json1['results'][1]['photos'][0]['photo_reference']+ '&key=AIzaSyBlOaCDL8ePD3nignTrJN1oViXj_rDx_1U'
 
 	
 	name3 = json1["results"][2]["name"]
@@ -61,21 +66,21 @@ def sceniclocations():
 	placeID3 = json1["results"][2]["place_id"]
 
 
-	photor3 = 'https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=' + json1['results'][2]['photos'][0]['photo_reference']+ '&key=AIzaSyD_XJoBX5jhOnTthWnFc1gzJYQ3sumP-pk'
+	photor3 = 'https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=' + json1['results'][2]['photos'][0]['photo_reference']+ '&key=AIzaSyBlOaCDL8ePD3nignTrJN1oViXj_rDx_1U'
 	return flask.render_template('products.html', name1=name1, placeID1= placeID1, rating1=rating1, photo1=photor1, name2=name2, placeID2=placeID2, rating2=rating2, photo2=photor2, name3=name3, placeID3=placeID3, rating3=rating3, photo3=photor3)
 
 @APP.route('/scenic/<placeID>')
 def scenicdetails(placeID):
-	r1 = requests.get('https://maps.googleapis.com/maps/api/place/details/json?placeid=' + placeID + '&key=AIzaSyD_XJoBX5jhOnTthWnFc1gzJYQ3sumP-pk')
+	r1 = requests.get('https://maps.googleapis.com/maps/api/place/details/json?placeid=' + placeID + '&key=AIzaSyBlOaCDL8ePD3nignTrJN1oViXj_rDx_1U')
 	json1 = r1.json()
-
-	name = json1["result"]["name"]
+	print(json1)
+	name = json1['result']['name']
 
 	address = json1["result"]["formatted_address"]
-	photo = 'https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=' + json1['result']['photos'][0]['photo_reference']+ '&key=AIzaSyD_XJoBX5jhOnTthWnFc1gzJYQ3sumP-pk'
+	photo = 'https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=' + json1['result']['photos'][0]['photo_reference']+ '&key=AIzaSyBlOaCDL8ePD3nignTrJN1oViXj_rDx_1U'
+	src_for_map = "https://www.google.com/maps/embed/place?key=AIzaSyBlOaCDL8ePD3nignTrJN1oViXj_rDx_1U&origin=place_id:" + placeID + "&output=embed"
 
-
-	return flask.render_template('scenicdetails.html', name=name, address=address, photo=photo)
+	return flask.render_template('scenicdetails.html', name=name, address=address, photo=photo, src_for_map=src_for_map)
 
 
 
