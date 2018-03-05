f0a6030a0512dfda2e0e6a2955733488a0c34b70
Amrutha Sreedharane <amrutha.sreedharane@utexas.edu>
2018-03-05 13:44:07 -0600

    Found missing files

---
+++ app/GooglePlaces.py
@@ -0,0 +1,60 @@
+from __future__ import print_function
+import ScenicLocations
+import flask
+
+import argparse
+import json
+import pprint
+import requests
+import sys
+import urllib
+import yelp
+import snapshots
+import githubstats
+import GooglePlaces
+
+
+list_locs = []
+
+def get_places():
+	r1 = requests.get('https://maps.googleapis.com/maps/api/place/textsearch/json?location=30.267153,-97.7430608&radius=10000&type=park&keyword=natural_feature&key=AIzaSyBlOaCDL8ePD3nignTrJN1oViXj_rDx_1U')
+	json1 = r1.json()
+	
+	print(json1)
+
+	#location3 = json1["results"][2]["formatted_address"]
+	placeID1 = json1["results"][0]["place_id"]
+	placeID2 = json1["results"][1]["place_id"]
+	placeID3 = json1["results"][2]["place_id"]
+	list_of_locs(placeID1)
+	list_of_locs(placeID2)
+	list_of_locs(placeID3)
+	return list_locs
+
+def list_of_locs(placeID):
+	r1 = requests.get('https://maps.googleapis.com/maps/api/place/details/json?placeid=' + placeID + '&key=AIzaSyBlOaCDL8ePD3nignTrJN1oViXj_rDx_1U')
+	json1 = r1.json()
+	print(json1)
+	try:
+		rating =  json1["result"]["rating"]
+
+	except:
+		rating = 0
+
+	
+	name = json1['result']['name']
+
+	address = json1["result"]["formatted_address"]
+	rating= json1["result"]["rating"]
+	photo = 'https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=' + json1['result']['photos'][0]['photo_reference']+ '&key=AIzaSyBlOaCDL8ePD3nignTrJN1oViXj_rDx_1U'
+	#src_for_map = "https://www.google.com/maps/embed/place?key=AIzaSyBlOaCDL8ePD3nignTrJN1oViXj_rDx_1U&origin=place_id:" + placeID + "&output=embed"
+	reviews = json1["result"]["reviews"]
+
+	scl = ScenicLocations(name, placeID, photo, rating, reviews, address)
+
+	list_locs.append(scl)
+
+
+
+
+
---
+++ app/ScenicLocations.py
@@ -0,0 +1,69 @@
+from __future__ import print_function
+from GooglePlaces import GooglePlaces
+import argparse
+import json
+import pprint
+import requests
+import sys
+import urllib
+
+class ScenicLocations:
+	def __init__(self, name, placeID, photo, rating, reviews, address) :
+		self.__name = name
+		self.__placeID = placeID
+		self.__photo = photo
+		self.__rating = rating
+		self.__reviews = reviews
+		self.__address = address
+
+	def __str__(self):
+		return str(self.__name)
+
+	@property
+	def name(self) :
+	    return self.__name
+
+	@name.setter
+	def name(self, n) :
+	    self.__name = n
+
+	@property
+	def placeId(self) :
+	    return self.__placeID
+
+	@placeId.setter
+	def placeID(self, placeID) :
+	    self.__placeID = placeID
+
+	@property
+	def photo(self) :
+	    return self.__photo
+
+	@photo.setter
+	def photo(self, photo) :
+	    self.__photo = photo
+
+	@property
+	def rating(self) :
+	    return self.__rating
+
+	@rating.setter
+	def rating(self, rating) :
+	    self.__rating = rating
+
+	@property
+	def reviews(self) :
+	    return self.__reviews
+
+	@reviews.setter
+	def reviews(self, reviews) :
+	    self.__reviews = reviews
+
+
+	@property
+	def address(self) :
+	    return self.__address
+
+	@address.setter
+	def address(self, address) :
+	    self.__address = address
