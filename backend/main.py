9f106d074f1af56db281131f1e31779d8acee506
Amrutha Sreedharane <amrutha.sreedharane@utexas.edu>
2018-04-19 13:55:00 -0500

    Added comments to main.oy

--- backend/main.py
+++ backend/main.py
@@ -50,45 +50,53 @@ def alchemyencoder(obj):
 def home():
     return flask.render_template('home.html')
 
+#Reroutes to API to get cities
 @APP.route('/getcities', methods=['GET'])
 def get_cities():
     return redirect('//api.espressoyoself.me/getcities', code=302)
 
-
+#Reroutes to path
 @APP.route('/<path:path>')
 def catch_all (path):
     return flask.render_template('home.html')
 
+#Reroutes to API to get scenic locations 
 @APP.route('/getsceniclocations', methods=['GET'])
 def get_sceniclocations() :
     return redirect('//api.espressoyoself.me/sceniclocations', code=302)
 
+#Reroutes to API to a scenic location by the scenic id
 @APP.route('/getsceniclocation/<scenicId>', methods=['GET'])
 def get_sceniclocation(scenicId) :
 
     return redirect('//api.espressoyoself.me/sceniclocation/' + scenicId, code=302)
 
+#Reroutes to API to get coffeeshops
 @APP.route('/getcoffeeshops', methods=['GET'])
 def get_coffeeshops() :
 
     return redirect('//api.espressoyoself.me/coffeeshops', code=302)
 
+#Reroutes to API to get a coffeeshop by the shop id
 @APP.route('/getcoffeeshop/<coffeeId>', methods=['GET'])
 def get_coffeeshop(coffeeId) :
 
     return redirect('//api.espressoyoself.me/coffeeshop/' + coffeeId, code=302)
 
+#Reroutes to API to get snapshots
 @APP.route('/getsnapshots', methods=['GET'])
 def get_snapshots() :
 
     return redirect('//api.espressoyoself.me/snapshots', code=302)
 
+#Reroutes to API to get a snapshot by the shap id
 @APP.route('/getsnapshot/<snapshotId>', methods=['GET'])
 def get_snapshot(snapshotId) :
 
     return redirect('//api.espressoyoself.me/snapshot/' + snapshotId, code=302)
 
 
+#Reroutes to API to get data based on a search key
 @APP.route('/search/<searchkey>',  methods=['GET'])
 def search(searchkey):
     print(searchkey)
@@ -97,20 +105,25 @@ def search(searchkey):
 
 
 #NEARBY SEARCHES
+
+#Reroutes to API to get scenic locations close to a shop by the shop id
 @APP.route('/nearby_scenic_from_shops/<shop_id>',  methods=['GET'])
 def nearby_scenic_from_shops(shop_id):
     return redirect('//api.espressoyoself.me/nearby_scenic_from_shops/' + shop_id, code=302)
 
 
+#Reroutes to API to get shops close to a scenic location by the location id
 @APP.route('/nearby_shops_from_scenic/<scenic_id>',  methods=['GET'])
 def nearby_shops_from_scenic(scenic_id):
     return redirect('//api.espressoyoself.me/nearby_shops_from_scenic/' + scenic_id, code=302)
 
 
+#Reroutes to API to get the coffeeshop at which the snap was taken
 @APP.route('/snapshots_shop/<shop_id>',  methods=['GET'])
 def snapshots_shop(shop_id):
     return redirect('//api.espressoyoself.me/snapshots_shop/' + shop_id, code=302)
 
+#Reroutes to API to get the scenic location at which the snap was taken
 @APP.route('/snapshots_scenic/<scenic_id>',  methods=['GET'])
 def snapshots_scenic(scenic_id):
 
